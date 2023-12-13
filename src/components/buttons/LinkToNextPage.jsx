1ab9cb7bc950a98cc294ad76b0ce5d7f3fe438c5
박윤수 <ys1008p@gmail.com>
2023-12-13 18:14:19 +0900

    fix: a태그 스타일 중복 수정

--- src/components/buttons/LinkToNextPage.jsx
+++ src/components/buttons/LinkToNextPage.jsx
@@ -3,7 +3,7 @@ import styles from "./LinkToNextPage.module.scss";
 
 export function LinkToNextPage({ children, isLandingPage }) {
   return (
-    <Link to={isLandingPage ? "/list" : "/post"}>
+    <Link className={styles.Link} to={isLandingPage ? "/list" : "/post"}>
       <button className={styles.Link__button}>{children}</button>
     </Link>
   );
