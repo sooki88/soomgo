e6276d98f0ee9cb53df5287b4cce8b4cce488109
박윤수 <ys1008p@gmail.com>
2023-12-13 15:01:00 +0900

    feat:랜딩페이지 및 List페이지 이동 버튼 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage.jsx";
 import { ListPage } from "./pages/ListPage.jsx";
 import { PostPage } from "./pages/PostPage.jsx";
-import "./styles/rest.css";
+import "./styles/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
