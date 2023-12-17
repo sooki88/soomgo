9c97e31964aca22a28cbf73737e929a6877225b5
shinseokjin <ssj9685@naver.com>
2023-12-17 15:52:18 +0900

    refactor: remove unused life cycles

--- src/pages/PostByIdPage.jsx
+++ src/pages/PostByIdPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useGetPostById } from "../post-by-id/data-access-post-by-id/useGetPostById";
 import { CardByIdList } from "../components/card-by-id-list/CardByIdList";
 import { CardById } from "../components/card-by-id/CardById";
@@ -7,16 +7,10 @@ import { Modal } from "../post-by-id/modal/Modal";
 const SAMPLE = 1088;
 
 export const PostByIdPage = ({ recipientId = SAMPLE }) => {
-  // 질문: 왜 prop의 recipientId를 {} 넣어야 작동을 할까요?
-
-  const [posts, setPosts] = useState(null);
   const [clickedPost, setClickedPost] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const { data } = useGetPostById(recipientId);
-
-  const sortedData = () => {
-    setPosts(data?.results.sort((a, b) => b.createdAt - a.createdAt));
-  };
+  const posts = data?.results;
 
   const handleClick = (post) => {
     setClickedPost(post.id);
@@ -27,21 +21,14 @@ export const PostByIdPage = ({ recipientId = SAMPLE }) => {
     setShowModal(false);
   };
 
-  useEffect(() => {
-    sortedData();
-  }, [recipientId]);
-
   return (
     <div>
       {showModal && (
         <Modal
-          post={data?.results.find((post) => post.id === clickedPost)}
+          post={posts?.find((post) => post.id === clickedPost)}
           onClose={handleClose}
         />
       )}
-
-      {/* 질문1: onClick과 onClose의 차이
-      질문2: 왜 어떤건 onClick={()=>}이고 어떤건 onClick={}인가요? */}
 
       <CardByIdList>
         {posts?.map((post) => (
